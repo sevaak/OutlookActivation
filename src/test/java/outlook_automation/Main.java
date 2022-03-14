13effedf1ef62b1a7a6e5f16d3801b7a24e7b436
sevak <sevak17@gmail.com>
2022-03-14 15:29:05 +0400

    Updated Project Commit

---
+++ src/test/java/outlook_automation/Main.java
@@ -0,0 +1,14 @@
+package test.java.outlook_automation;
+
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+
+public class Main {
+    public static void main(String[] args) {
+        TestListenerAdapter tla = new TestListenerAdapter();
+        TestNG testng = new TestNG();
+        testng.setTestClasses(new Class[]{outlook_automation.OutlookActivation.class});
+        testng.addListener(tla);
+        testng.run();
+    }
+}
